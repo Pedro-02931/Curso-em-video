60a99937dfd36f2ae190f0242549e94a39bd0a9e
Pedro <pedromotad9@gmail.com>
2021-05-26 20:31:52 -0300

    Create Três leis principais.java

---
+++ Curso de Java/Três leis principais.java
@@ -0,0 +1,8 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("1. Um robo nunca pode ferir um humano ou permitir que algum humano sofra algum mau");
+		System.out.println("2. Os robos devem obedecer às ordens humanos, exceto nos casos em que tais ordens entre em conflito com a primeira lei;");
+	    System.out.println("3. Um robo deve proteger sua própria existência, desde que não entre em conflito com as leis anteriores");
+	}
+}
